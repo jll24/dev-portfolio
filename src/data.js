74d50026ca677b2aca99b8b199f9f415383518f8
Lester Lacson <iamjohnlacson@gmail.com>
2021-12-29 06:17:00 +0800

    Added: website links

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 export const products = [
     {
       id: 1,
-      title: "To-Do",
+      title: "Wise Planner",
       img: "https://res.cloudinary.com/jllacson/image/upload/v1640702028/sampleShop/screencapture-localhost-3000-2021-12-28-22_33_29_bageqb.png",
-      link: "http://lama.dev",
+      link: "https://wise-planner.herokuapp.com/",
       source: "https://github.com/jll24/uplift-todo-app",
-      desc: "A wise man is a man who plans ahead, and uses this to-do app. Developed using React-Redux, CSS, and Framer Motion for animation."
+      desc: "A wise man is a man who plans ahead, and uses this app. Developed using React-Redux, CSS, and Framer Motion for animation."
 
     },
     {
       id: 2,
       title: "Taste Restaurant",
       img: "https://res.cloudinary.com/jllacson/image/upload/v1640706041/sampleShop/screencapture-localhost-3000-2021-12-28-22_35_08_cjz9wx.png",
-      link: "http://lama.dev",
+      link: "https://taste-resto.herokuapp.com/",
       source: "https://github.com/jll24/uplift-food-app",
       desc: "Satisfy your food cravings with this food app. Developed using React-Redux, CSS."
 
@@ -21,7 +21,7 @@ export const products = [
       id: 3,
       title: "hiWORLD.",
       img: "https://res.cloudinary.com/jllacson/image/upload/v1640702766/sampleShop/screencapture-localhost-3000-2021-12-28-22_45_46_zxuvti.png",
-      link: "http://lama.dev",
+      link: "https://hi-wrld.herokuapp.com/",
       source: "https://github.com/jll24/uplift-ecommerce-app",
       desc: "An e-commerce app for high-end fashion brands. A MERN Stack Application with Stripe Payment Integration."
 
@@ -30,7 +30,7 @@ export const products = [
       id: 4,
       title: "AltitudeFX",
       img: "https://res.cloudinary.com/jllacson/image/upload/v1640706221/sampleShop/screencapture-file-C-Users-Lindsay-Javier-Documents-Lacson-uplift-static-app-index-html-2021-12-28-22_12_15_muxi8m.png",
-      link: "https://altitude-fx.herokuapp.com/",
+      link: "https://jll24.github.io/uplift-static-app/",
       source: "https://github.com/jll24/uplift-static-app",
       desc: "A forex trading static website with a modern approach in UI. Developed using HTML and CSS."
     },
@@ -38,7 +38,7 @@ export const products = [
       id: 5,
       title: "322 Play",
       img: "https://res.cloudinary.com/jllacson/image/upload/v1640704930/sampleShop/screencapture-file-C-Users-Lindsay-Javier-Documents-Lacson-uplift-game-app-mainPage-index-html-2021-12-28-22_14_47_uwix1q.png",
-      link: "http://lama.dev",
+      link: "https://jll24.github.io/uplift-game-app/",
       source: "https://github.com/jll24/uplift-game-app",
       desc: "322 Play is a 2-in-1 game app inspired by online casino websites. Consists of 2 a lottery and a slot machine. Developed using HTML, CSS, and JavaScript."
     },
@@ -46,7 +46,7 @@ export const products = [
       id: 6,
       title: "Pokédex",
       img: "https://res.cloudinary.com/jllacson/image/upload/v1640706359/sampleShop/screencapture-localhost-3000-2021-12-28-22_23_10_emzh1l.png",
-      link: "http://lama.dev",
+      link: "https://po-ke-dex.herokuapp.com/",
       source: "https://github.com/jll24/uplift-pokedex-app",
       desc: "Gotta catch and LIST 'em all! Developed using React-Redux, CSS."
 
